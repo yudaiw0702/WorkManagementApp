34b5ad42a10e73c4561b3fff7fbbab5c1cbc0dd6
yudai <yudai.0702wa@gmail.com>
2020-05-29 12:05:42 +0900

    設定画面を開けるようにした

--- WorkManagementApp/MainWindow.xaml.cs
+++ WorkManagementApp/MainWindow.xaml.cs
@@ -46,12 +46,63 @@ namespace WorkManagementApp
         private Gesture seat;
         private Gesture right_playphone;
 
+        //タイマー
+        DispatcherTimer dispatcherTimer;    // タイマーオブジェクト
+        int TimeLimit = 30;                 // 制限時間
+        DateTime StartTime;                 // カウント開始時刻
+        TimeSpan nowtimespan;               // Startボタンが押されてから現在までの経過時間
+        TimeSpan oldtimespan;               // 一時停止ボタンが押されるまでに経過した時間の蓄積
+
         public MainWindow()
         {
             InitializeComponent();
 
             Loaded += MainWindow_Loaded;
             Closing += MainWindow_Closing;
+
+            // コンポーネントの状態を初期化　
+            lblTime.Content = "00:00:000";
+
+            // タイマーのインスタンスを生成
+            dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+        }
+
+        // タイマー Tick処理
+        void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            nowtimespan = DateTime.Now.Subtract(StartTime);
+            lblTime.Content = oldtimespan.Add(nowtimespan).ToString(@"mm\:ss\:fff");
+
+            if (TimeSpan.Compare(oldtimespan.Add(nowtimespan), new TimeSpan(0, 0, TimeLimit)) >= 0)
+            {
+                TimerStop();
+                TimerReset();
+                MessageBox.Show(String.Format("{0}秒経過しました。", TimeLimit),
+                                "Infomation", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        // タイマー操作：開始
+        private void TimerStart()
+        {
+            StartTime = DateTime.Now;
+            dispatcherTimer.Start();
+        }
+
+        // タイマー操作：停止
+        private void TimerStop()
+        {
+            oldtimespan = oldtimespan.Add(nowtimespan);
+            dispatcherTimer.Stop();
+        }
+
+        // タイマー操作：リセット
+        private void TimerReset()
+        {
+            oldtimespan = new TimeSpan();
+            lblTime.Content = "00:00:000";
         }
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
@@ -194,9 +245,19 @@ namespace WorkManagementApp
                 if (gestureFrame != null && gestureFrame.DiscreteGestureResults != null)
                 {
                     //Discrete
-                    var result = gestureFrame.DiscreteGestureResults[seat];
-                    var result1 = gestureFrame.DiscreteGestureResults[right_playphone];
+                    var resultSeat = gestureFrame.DiscreteGestureResults[seat];
+                    var resultRPP = gestureFrame.DiscreteGestureResults[right_playphone];
                     
+                    //作業してるとき（座っている動作）
+                    if (0.9 < resultSeat.Confidence)
+                    {
+                        // タイマー開始
+                        TimerStart();
+                    }
+                    else
+                    {
+                        TimerStop();
+                    }
                 }
             }
         }
@@ -206,6 +267,12 @@ namespace WorkManagementApp
         {
             StateWindow sw = new StateWindow();
             sw.Show();
+        }
+
+        private void Config_open_Click(object sender, RoutedEventArgs e)
+        {
+            ConfigWindow cw = new ConfigWindow();
+            cw.Show();
         }
     }
 }
